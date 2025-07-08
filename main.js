06b2b6f55441939d55baad6e9632e8ad5dfb2a6a
mirkobechini <mirkobechini@gmail.com>
2025-07-08 21:31:29 +0200

    completed

--- main.js
+++ main.js
@@ -3,4 +3,19 @@
 //controllare se il numero è divisibile per 5 -> stampare buzz
 //controllare se il numero è divisibile per 3 e per 5 -> stampare fizzbuzz
 
+let msg = "";
 
+for (let i = 1; i < 101; i++) {
+    if (i % 5 != 0 && i % 3 != 0) {
+        msg = i;
+    } else {
+        if (i % 3 == 0) {
+            msg += "Fizz";
+        }
+        if (i % 5 == 0) {
+            msg += "Buzz";
+        }
+    }
+    console.log(`${msg}\n`);
+    msg = "";
+}

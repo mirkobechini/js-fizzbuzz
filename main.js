c800e0ab91a5741b5485cd5420cdfaee8cacf11e
mirkobechini <mirkobechini@gmail.com>
2025-07-08 21:24:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+//Creare un ciclo che parta da 1 e arrivi a 100 compresi
+//controllare se il numero è divisibile per 3 -> stampare fizz
+//controllare se il numero è divisibile per 5 -> stampare buzz
+//controllare se il numero è divisibile per 3 e per 5 -> stampare fizzbuzz
+
+
